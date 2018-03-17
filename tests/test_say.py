5ce893c36794629212f3c4ade8e2c62d77850a58
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-03-18 01:01:38 +0900

    test_say.py

--- tests/test_say.py
+++ tests/test_say.py
@@ -1,1 +1,14 @@
-test_say.py
+# mainディレクトリのsay.pyのFooクラスとHogeクラスを呼び出す
+from main.say import Foo, Hoge
+
+def test_foo_say():
+	assert Foo().say() == 'foo'
+
+def test_foo_say2():
+	assert Foo().say2() == 'foo' # わざとエラー
+
+def test_hoge_say():
+	assert 'hoge'
+
+def test_hoge2_say():
+	assert 'hoge2'
