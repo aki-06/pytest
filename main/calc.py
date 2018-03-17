ba7e08c8400ccdde695286997f9bf391f4b48c8e
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-03-18 00:43:34 +0900

    calc.py

---
+++ main/calc.py
@@ -0,0 +1,17 @@
+class Calc:
+
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def add(self):
+		return self.x + self.y
+
+	def dif(self):
+		return self.x - self.y
+
+	def seki(self):
+		return self.x * self.y
+
+	def shou(self):
+		return self.x / self.y
