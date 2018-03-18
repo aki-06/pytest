6cc771dd771cfac64cef9fbce1566871f44ed06e
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-03-18 10:00:23 +0900

    fuxed

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,5 +1,6 @@
+import pytest
 # mainディレクトリのcalc.pyのCalcクラスを呼び出す
-from main.calc import Calc
+from sample.calc import Calc
 
 def test_add_01():
 	assert Calc(9, 2).add() == 11
@@ -8,19 +9,19 @@ def test_add_02():
 	assert Calc(-9, 2).add() == -7
 
 def test_dif_01():
-	assert Calc(9, 2) == 7
+	assert Calc(9, 2).dif() == 7
 
 def test_dif_02():
-	assert Calc(-9, 2) == -11
+	assert Calc(-9, 2).dif() == -11
 
 def test_seki_01():
-	assert Calc(9, 2) == 18
+	assert Calc(9, 2).seki() == 18
 
 def test_seki_02():
-	assert Calc(-9, 2) == -18
+	assert Calc(-9, 2).seki() == -18
 
 def test_shou_01():
-	assert Calc(9, 2) == 4.5
+	assert Calc(9, 2).shou() == 4.5
 
 def test_shou_02():
-	assert Calc(-9, 2) == -4.5
+	assert Calc(-9, 2).shou() == -4.5
--- tests/test_say.py
+++ tests/test_say.py
@@ -1,11 +1,13 @@
+import pytest
 # mainディレクトリのsay.pyのFooクラスとHogeクラスを呼び出す
-from main.say import Foo, Hoge
+from sample.say import Foo, Hoge
 
 def test_foo_say():
 	assert Foo().say() == 'foo'
 
 def test_foo_say2():
-	assert Foo().say2() == 'foo' # わざとエラー
+	assert Foo().say() == 'foo' # わざとエラー
+	# assert Foo().say2() == 'foo2'
 
 def test_hoge_say():
 	assert 'hoge'
